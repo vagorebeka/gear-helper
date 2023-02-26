8e22b6e3608fde5dd7c0e3488dbc4a5105bc0358
Vágó Rebeka <79878482+vagorebeka@users.noreply.github.com>
2023-02-26 23:38:46 +0100

    Model created - CharacterClass

---
+++ app/Models/CharacterClass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CharacterClass extends Model
+{
+    use HasFactory;
+
+    protected $visible = [
+        'id',
+        'name',
+        'stat1',
+        'stat2',
+        'stat3'
+    ];
+
+    protected $fillable = [
+        'name',
+        'stat1',
+        'stat2',
+        'stat3'
+    ];
+}
