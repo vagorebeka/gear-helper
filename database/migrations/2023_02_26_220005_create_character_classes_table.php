46b00a6084104430a68a5f7042150c98632b29ad
Vágó Rebeka <79878482+vagorebeka@users.noreply.github.com>
2023-02-26 23:51:57 +0100

    Create table done - CharacterClass

--- database/migrations/2023_02_26_220005_create_character_classes_table.php
+++ database/migrations/2023_02_26_220005_create_character_classes_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('character_classes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('stat1');
+            $table->string('stat2');
+            $table->string('stat3');
+            $table->string('stat4');
+            $table->string('stat5');
             $table->timestamps();
         });
     }
