7c60e21bd69a8aff4daf0a171c1f57ce79b8ae1c
Vágó Rebeka <79878482+vagorebeka@users.noreply.github.com>
2023-04-09 22:17:38 +0200

    User seeding done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,10 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            'username' => fake()->name(),
+            'username' => fake()->unique()->firstName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -29,7 +29,7 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
+    public function unverified()
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,12 +14,15 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        DB::table('users')->insert([
-            'username' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
+        User::factory(5)->create();
     }
+    // {
+    //     DB::table('users')->insert([
+    //         'username' => Str::random(10),
+    //         'email' => Str::random(10).'@gmail.com',
+    //         'password' => Hash::make('password'),
+    //     ]);
+    // }
 }
